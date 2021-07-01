dbed4f6602b104eae70bfc3829f3cd571059dd1d
Murat Akif Dumlu <murat.akif@coffeapp.com>
2021-07-01 23:30:03 +0300

    Refactor SyncCalendar worker

--- app/workers/sync_calendar_worker.rb
+++ app/workers/sync_calendar_worker.rb
@@ -5,8 +5,8 @@ class SyncCalendarWorker
   sidekiq_options queue: :calendar_setup, retry: 3
 
   def perform(user_id)
-    last_checked_in = Event.find_by(user_id: user_id, order: 'starts_at DESC')&.starts_at # TODO: add last_checked_in column to users and query through there
-    service = Events::Sync.new(user_id, from)
+    last_checked_in = User.select(:last_checked_in).find(user_id)&.last_checked_in
+    service = Events::Sync.new(user_id, from: last_checked_in)
     service.call
   end
 end
